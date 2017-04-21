28e5b32059fd6ac1be5dc5ce64d91fa10bc726fc
Aaron Pantoja <pantojaaron@gmail.com>
2017-04-21 09:54:28 -0700

    Show timestamp on UI

--- moment-examples.js
+++ moment-examples.js
@@ -1,9 +1,9 @@
 var moment = require('moment');
 var now = moment();
 
-// console.log(now.format());
-// console.log(now.format('X'));
-// console.log(now.valueOf()); //javascript timestamp, not epoch timestamp
+console.log(now.format());
+console.log(now.format('X'));
+console.log(now.valueOf()); //javascript timestamp, not epoch timestamp
 
 var timestamp = 1492763741506;
 
@@ -11,7 +11,7 @@ var timestampMoment = moment.utc(timestamp);
 
 console.log(timestampMoment.local().format('h:mm a'));
 
-// now.subtract(1, 'year');
+now.subtract(1, 'year');
 
-// console.log(now.format());
-// console.log(now.format('MMM Do YYYY, h:mma'));
+console.log(now.format());
+console.log(now.format('MMM Do YYYY, h:mma'));
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,19 @@
 var socket = io();
 
+// var time = moment().valueOf();
+// var timestampMoment = moment.utc(time);
+// var timestamp = timestampMoment.local().format('h:mm a');
+
 socket.on('connect', function () {
     console.log('Connected to socket.io server!');
 });
 
 socket.on('message', function (message) {
+    var momentTimestamp = moment.utc(message.timestamp);
     console.log('New message:');
     console.log(message.text);
 
-    jQuery('.messages').append('<p>' + message.text + '</p>');
+    jQuery('.messages').append('<p>' + '<strong>' + momentTimestamp.local().format('h:mm a')+ '</strong>' + ': ' + message.text + '</p>');
 });
 
 // Handles submitting of new message
